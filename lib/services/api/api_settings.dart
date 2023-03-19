aec36dbcfc76c2316d15899a7dec2ac99e4caf65
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-03-19 10:41:49 +0530

    Added GetHeaders method in ApiSettings class

--- lib/services/api/api_settings.dart
+++ lib/services/api/api_settings.dart
@@ -1,3 +1,16 @@
+import 'package:glancefrontend/services/local_storage.dart';
+
 class ApiSettings {
   static const String baseUrl = "glance.sathiyaraman-m.com";
+
+  static Future<Map<String, String>> getHeaders() async {
+    final headers = {
+      'Content-Type': 'application/json; x-api-version=1.0; charset=UTF-8',
+    };
+    final authToken = await LocalStorage.getAuthToken();
+    if (authToken != null) {
+      headers['Authorization'] = 'Bearer $authToken';
+    }
+    return headers;
+  }
 }
