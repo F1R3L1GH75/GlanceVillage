5b794e8aacc3e096d9dc6fa9bdbe6d08a0506ec8
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-04-23 21:23:57 +0530

    Updated JobCardDetailScreen

--- lib/screens/jobcards/jobcard_detail_screen.dart
+++ lib/screens/jobcards/jobcard_detail_screen.dart
@@ -2,85 +2,97 @@ import 'package:flutter/material.dart';
 import 'package:glancefrontend/models/jobcards/jobcard_response.dart';
 import 'package:glancefrontend/screens/jobcards/jobcard_detailed_screen.dart';
 import 'package:glancefrontend/services/api/jobcard_service.dart';
+import 'package:provider/provider.dart';
 
-class JobCardDetailScreen extends StatefulWidget {
+class JobCardDetailScreen extends StatelessWidget {
   const JobCardDetailScreen({super.key, required this.jobCardId});
 
   final String? jobCardId;
 
   @override
-  State<JobCardDetailScreen> createState() => _JobCardDetailScreenState();
-}
-
-class _JobCardDetailScreenState extends State<JobCardDetailScreen> {
-  late Future<JobCardResponse> _jobCard;
-
-  @override
-  void initState() {
-    super.initState();
-    _jobCard = JobCardService.getJobCardByIdAsync(widget.jobCardId!);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<JobCardResponse>(
-        future: _jobCard,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return JobCardDetailedScreen(jobCard: snapshot.data!);
-            /*return Scaffold(
+    return ChangeNotifierProvider<JobCardDetailScreenState>(
+      create: (_) => JobCardDetailScreenState(jobCardId),
+      builder: (context, child) {
+        final provider = Provider.of<JobCardDetailScreenState>(context);
+        final jobCard = provider.jobCard;
+        if (provider._isLoading == false) {
+          if (provider.isError == true) {
+            return Scaffold(
               appBar: AppBar(
                 centerTitle: true,
-                title: Text('Job Card Detail: ${snapshot.data!.jobCardNumber}'),
+                title: const Text('Job Card Detail: Error'),
                 backgroundColor: const Color(0xFF2661FA),
               ),
-              body: SingleChildScrollView(
-                child: Center(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(18),
-                        child: Column(
-                          children: [
-                            Card(
-                              child: Column(
-                                children: [
-                                  ListTile(
-                                    title: Text(
-                                        'Job Card Number: ${snapshot.data!.jobCardNumber}'),
-                                    subtitle: Text(
-                                        'Job Card Name: ${snapshot.data!.name}'),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Error: ${provider.errorMessage}'),
+                    MaterialButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        color: const Color(0xFF2661FA),
+                        textColor: Colors.white,
+                        child: const Text('Go Back'))
+                  ],
                 ),
               ),
-            );*/
-          } else if (snapshot.hasError) {
-            return Scaffold(body: Center(child: Text('${snapshot.error}')));
+            );
           } else {
-            return Scaffold(
-                appBar: AppBar(
-                  title: const Text('Loading...'),
-                  backgroundColor: const Color(0xFF2661FA),
-                ),
-                body: SafeArea(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                      Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: CircularProgressIndicator()),
-                      Text('Loading Job Card Details...')
-                    ])));
+            return JobCardDetailedScreen(jobCard: jobCard!);
           }
-        });
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Job Card Detail: Loading'),
+              backgroundColor: const Color(0xFF2661FA),
+            ),
+            body: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class JobCardDetailScreenState with ChangeNotifier {
+  JobCardResponse? _jobCard;
+  JobCardResponse? get jobCard => _jobCard;
+  bool _isLoading = true;
+  bool get isLoading => _isLoading;
+
+  bool _isError = false;
+  bool get isError => _isError;
+
+  String _errorMessage = '';
+  String get errorMessage => _errorMessage;
+
+  final String? jobCardId;
+
+  JobCardDetailScreenState(this.jobCardId) {
+    _isLoading = true;
+    getJobCardByIdAsync();
+  }
+
+  Future<void> getJobCardByIdAsync() async {
+    _isLoading = true;
+    _isError = false;
+    _errorMessage = '';
+    try {
+      final jobCard = await JobCardService.getJobCardByIdAsync(jobCardId!);
+      _jobCard = jobCard;
+      _isLoading = false;
+    } catch (e) {
+      _isLoading = false;
+      _isError = true;
+      _errorMessage = e.toString();
+    }
+    notifyListeners();
   }
 }
