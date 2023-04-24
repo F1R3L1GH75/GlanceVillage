237094cbcc45c85bb921cd7bb42fd1fe9a666249
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-04-24 06:11:05 +0530

    Update Layout for SelectSearchTypeDialog

--- lib/screens/jobcards/dialogs/select_searchtype_dialog.dart
+++ lib/screens/jobcards/dialogs/select_searchtype_dialog.dart
@@ -11,23 +11,23 @@ class _SelectSearchTypeDialogState extends State<SelectSearchTypeDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        alignment: Alignment.center,
-        child: dialogContent(context),
-      );
+      alignment: Alignment.center,
+      child: dialogContent(context),
+    );
   }
 
   dialogContent(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20),
-      height: 230,
+      padding: const EdgeInsets.all(16),
+      height: 110,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             alignment: Alignment.center,
             child: const Text(
-              "Select Search Type",
+              "Search using",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -35,28 +35,36 @@ class _SelectSearchTypeDialogState extends State<SelectSearchTypeDialog> {
             ),
           ),
           SizedBox(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context, "Job Card Number");
-                  },
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(40),
-                    backgroundColor: const Color(0xFF2661FA),
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context, "Job Card Number");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF2661FA),
+                    ),
+                    child: const Text("Job Card Number"),
                   ),
-                  child: const Text("Job Card Number"),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context, "Manual Search");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(40),
-                    backgroundColor: const Color(0xFF2661FA),
+                const SizedBox(width: 5),
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context, "Manual Search");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF2661FA),
+                    ),
+                    child: const Text("Manual Search"),
                   ),
-                  child: const Text("Manual Search"),
                 ),
               ],
             ),
